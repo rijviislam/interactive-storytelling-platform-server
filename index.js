ebf4fc870277c171a8ac296091e6546127887389
Rijvi Islam <rijviislam54@gmail.com>
2024-09-06 21:29:12 +0600

    feat: added all story and get path method

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ require("dotenv").config();
 const port = process.env.PORT || 5001;
 
 const corsOptions = {
-  origin: ["http://localhost:5001", "http://localhost:5174"],
+  origin: [
+    "http://localhost:5001",
+    "http://localhost:5173",
+    "http://localhost:5174",
+  ],
 };
 // MIDDLE-WARE //
 app.use(cors(corsOptions));
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yy4jwyq.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,7 +24,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,15 +33,83 @@ async function run() {
     // await client.connect();
     // Send a ping to confirm a successful connection
 
-    const postStoryCollection = client.db("interactive-storytelling-platform").collection("postStory");
+    const postStoryCollection = client
+      .db("interactive-storytelling-platform")
+      .collection("postStory");
+    const pathCollection = client
+      .db("interactive-storytelling-platform")
+      .collection("path");
 
-    app.post("/add-story", async(req,res) => {
+    // STORY POSTED //
+    app.post("/add-story", async (req, res) => {
+      try {
         const story = req.body;
         const result = await postStoryCollection.insertOne(story);
         res.send(result);
-    })
+      } catch (error) {
+        console.error("Error Posting data:", error);
+        res.status(500).send({ message: "Failed to Post data" });
+      }
+    });
+    // ADD PATH //
+    app.post("/add-path", async (req, res) => {
+      try {
+        const path = req.body;
+        const result = await pathCollection.insertOne(path);
+        res.send(result);
+      } catch (error) {
+        console.error("Error Posting data:", error);
+        res.status(500).send({ message: "Failed to Post data" });
+      }
+    });
+    // GET PATH //
+    app.get("/get-path", async (req, res) => {
+      try {
+        const result = await pathCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        res.status(500).send({ message: "Failed to fetch data" });
+      }
+    });
+    // GET STORY //
+    app.get("/all-story", async (req, res) => {
+      try {
+        const result = await postStoryCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        res.status(500).send({ message: "Failed to fetch data" });
+      }
+    });
+    // GET STORY DETAILS //
+    app.get("/story-details/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await postStoryCollection.findOne(query);
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        res.status(500).send({ message: "Failed to fetch data" });
+      }
+    });
+    app.get("/my-story/:email", async (req, res) => {
+      const myEmail = req.params.email.trim();
+      try {
+        const query = { email: myEmail };
+        const result = await postStoryCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        res.status(500).send({ message: "Failed to fetch data" });
+      }
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -48,9 +118,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("Job Task is Running!!");
-  });
-  
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+  res.send("Job Task is Running!!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
